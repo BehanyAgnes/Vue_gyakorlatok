9ccab9f8c5e6110ceeab068c5f3da0af0745b714
BehanyAgnes <agnesbehany22@gmail.com>
2025-11-27 17:07:18 +0100

    Delete Házi feladatok/WizzAzr_clone directory

--- Házi feladatok/WizzAzr_clone/src/views/Home.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <Hero />
-  <Destinations />
-</template>
-
-<script setup>
-import Hero from '../components/Hero.vue'
-import Destinations from '../components/Destinations.vue'
-</script>
